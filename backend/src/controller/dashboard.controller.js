6ab6a67615d0ce928fcfc31c2f9832e670a856ab
Jadhav Ajinkya Deepak <ajinkyajadhav.email@gmail.com>
2026-07-21 19:42:15 +0530

    Update dashboard.controller.js

--- backend/src/controller/dashboard.controller.js
+++ backend/src/controller/dashboard.controller.js
@@ -108,3 +108,39 @@ const activeUrls = asyncHandler(async (req, res) => {
 
     return res.status(200).json(new ApiResponse(200, result, "active urls fetched."))
 })
+
+const deactiveUrls = asyncHandler(async(req,res) => {
+    const {
+        page = 1,
+        limit = 10,
+        sortType = "desc"
+    } = req.query
+
+    const skip = (Number(page) - 1) * Number(limit)
+
+    const result = await Url.aggregate([
+        {
+            $match: {
+                userId: req.user._id,
+                isActive: false,
+                $or: [
+                    { expiresAt: null },
+                    { expiresAt: { $gt: new Date() } }
+                ]
+            }
+        },
+        {
+            $sort: {
+                createdAt: sortType == "asc" ? 1 : -1
+            }
+        },
+        {
+            $skip: skip
+        },
+        {
+            $limit: Number(limit)
+        }
+    ])
+
+    return res.status(200).json(new ApiResponse(200, result, "deactive urls fetched."))
+})
